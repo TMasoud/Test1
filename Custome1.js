43eb75bca08b90059c3999b3050f185e57043513
Masoud T <mtafavvoghi@yahoo.com>
2020-01-08 00:17:53 +0330

    SSS added

--- Custome1.js
+++ Custome1.js
@@ -2,7 +2,7 @@ function myfunc(){
     var a = document.forms['myForm']["myName"].value;
     console.log(a);
     if (a==""){
-        alert('Your name is empty!');
+        alert('Your name is Changed!');
         return false;
     }
 }
